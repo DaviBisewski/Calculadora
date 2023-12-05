509b12cc8a6aa445c567ddf9044de990865cc2e9
DaviBisewski <davimbisewski@gmail.com>
2023-12-05 20:20:21 -0300

    css arrumado

--- global.js
+++ global.js
@@ -1,22 +1,25 @@
+/*função inserir numero na tela de resultado*/
 function inserir(num){
-    document.querySelector(".tela").innerHTML += num;
-}
+    document.querySelector(".tela").innerHTML += num; /*função que vai inserir o número, parâmetro num, selecionar tela para manipular e devolver o num. Uso do mais igual para concatenar*/
+} 
+/*função limpar tela de resultado*/
 function limpar(){
-    document.querySelector(".tela").innerHTML = "";
+    document.querySelector(".tela").innerHTML = ""; /*Quando acionar a função vai apresentar nada para tela de resultado*/
 }
+/*função apagar numero a direita na tela de resultado*/
 function apagar(){
    let tela = document.querySelector(".tela").innerHTML;
-   document.querySelector(".tela").innerHTML = tela.substring(0, tela.length -1);
+   document.querySelector(".tela").innerHTML = tela.substring(0, tela.length -1); /*let tela vai guardar a .tela, depois document, buscar a .tela e passar o comando substring que vai extrair uma parte de uma string, e apagar 1 por vez*/
 }
 function calcular(){
-    let conta = document.querySelector(".tela").innerHTML;  
-    try{
-        let tela = eval(conta)
-        document.querySelector('.tela').innerHTML = tela;
+    let tela = document.querySelector(".tela").innerHTML;
+    document.querySelector('.tela').innerHTML = eval(tela) /*eval analisa a string fornecida e executa o código JavaScript contido nela,*/
+    if(tela){
+        document.querySelector('.tela').innerHTML = eval(tela) /*if, else para executar comando ou não*/
     }
-    catch(error){
-            document.querySelector('.tela').innerHTML = 'Erro';
-        }
+    else{
+        document.querySelector(".tela").innerHTML = "Erro"
     }
+}
 
 
